e31b497bdbff4966a0ae7f274de4539759709f87
pexshvela <gio.fexshvelashvili@gmail.com>
2026-03-01 14:03:11 +0400

    En Promocode Change 01.03

--- promobot.py
+++ promobot.py
@@ -71,7 +71,7 @@ DISCUSSION_GROUP_IDS = {
 # Promo codes per language – replace with your real promo codes
 # ---------------------------------------------------------------------------
 PROMO_CODES = {
-    "en": "NGHSFR",
+    "en": "SNGFH",
     "it": "ADJSDU",
     "fr": "OADIDMA",
     "mx": "ODLWDIM",
